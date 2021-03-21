b95e1346333ab0168df17a852da6d31980a58e82
tommyrharper <thomasrobertharper@gmail.com>
2021-03-21 21:11:23 +0000

    Fetch data in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const App = ({ props }) => (
-  <div>
-    Hello World
-  </div>
-);
+const App = () => {
+  const [response, setResponse] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('http://www.mocky.io/v2/5c62e7c33000004a00019b05')
+      .then((res) => res.json())
+      .then((res) => {
+        console.log('res', res);
+        setResponse(res);
+        setLoading(false);
+      })
+      .catch(() => setHasError(true));
+  }, []);
+  return (
+    <div>
+      Hello World
+      { hasError
+        && <p>An error occurred</p>}
+      { loading
+        && <div>Loading...</div>}
+    </div>
+  );
+};
 
 export default App;
