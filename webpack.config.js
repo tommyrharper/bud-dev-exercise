e39e8e910082ad0855ea73119a694d0fcde1d829
tommyrharper <thomasrobertharper@gmail.com>
2021-03-21 20:46:41 +0000

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = env => {
+  const devMode = env.NODE_ENV !== 'production';
+
+  return {
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].bundle.css',
+        chunkFilename: '[id].css'
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      open: true,
+      clientLogLevel: 'silent',
+      port: 9000,
+      historyApiFallback: true,
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx|js)$/,
+          include: path.resolve(__dirname, 'src'),
+          exclude: /node_modules/,
+          use: [{
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  "targets": "defaults" 
+                }],
+                '@babel/preset-react'
+              ]
+            }
+          }, {
+            loader: 'eslint-loader',
+            options: {
+              fix: true
+            }
+          }]
+        },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, 'src'),
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1 
+              }
+            },
+            'postcss-loader'
+          ]
+        }
+      ]
+    }
+  }
+}
