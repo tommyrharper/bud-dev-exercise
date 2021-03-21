07e23d03dfc22cdbb91743be72c34e2ff48fda2b
tommyrharper <thomasrobertharper@gmail.com>
2021-03-21 22:28:01 +0000

    Create ExpenseTable component

---
+++ src/components/ExpenseTable.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Expense from "./Expense";
+
+const ExpenseTable = ({ expenses }) => (
+  <table style={{ width: "100%" }}>
+    <tr>
+      <th>Date</th>
+      <th>Amount</th>
+      <th>Currency</th>
+      <th>Title</th>
+      <th>Description</th>
+    </tr>
+    {expenses.map((expense) => (
+      <Expense key={expense.id} expense={expense} />
+    ))}
+  </table>
+);
+
+export default ExpenseTable;
