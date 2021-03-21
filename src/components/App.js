323c91746df2bd1da4cd0870e27174a348d7211a
tommyrharper <thomasrobertharper@gmail.com>
2021-03-21 22:27:52 +0000

    Use ExpenseTable component in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import ExpenseTable from "./ExpenseTable";
 import useFetch from "../hooks/useFetch";
 
 const App = () => {
   const [response, loading, hasError] = useFetch(
     "http://www.mocky.io/v2/5c62e7c33000004a00019b05",
   );
-  console.log(`response`, response);
+
+  let smallestExpenses;
+  if (!loading && response) {
+    const { transactions } = response;
+    smallestExpenses = transactions
+      .filter((tx) => tx.amount.value < 0)
+      .sort((txa, txb) => txb.amount.value - txa.amount.value)
+      .slice(0, 10);
+  }
+  console.log(`smallestExpenses`, smallestExpenses);
+  const displayTransactions = !hasError && !loading && response;
 
   return (
     <div>
-      Hello World
       {hasError && <p>An error occurred</p>}
       {loading && <div>Loading...</div>}
+      {displayTransactions && <ExpenseTable expenses={smallestExpenses} />}
     </div>
   );
 };
