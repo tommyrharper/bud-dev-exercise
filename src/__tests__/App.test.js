af355c5cf02c00d78ebd77998a35ed5aeeca8b98
tommyrharper <thomasrobertharper@gmail.com>
2021-03-22 01:31:30 +0000

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,15 @@
-const sum = (a, b) => a + b;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import App from '../components/App';
 
-it('sums numbers', () => {
-  expect(sum(1, 2)).toEqual(3);
-  expect(sum(2, 2)).toEqual(4);
+it('Renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it('Renders a title', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.text().includes('Bud - 10 Smallest Expenses')).toBe(true);
 });
